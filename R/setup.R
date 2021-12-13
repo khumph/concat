2f0ea568a16063c54581cd01038d5e707ba8ea1b
Kyle Humphrey <khumph2@gmail.com>
2021-12-13 10:28:20 -0800

    Add more feedback to setup function

--- R/setup.R
+++ R/setup.R
@@ -1,9 +1,15 @@
 #' Install tinytex and required LaTeX packages
 #' @export
 concat_setup <- function(pkgs = c("geometry", "pdfpages", "hyperref")) {
-  message("Installing tinytex and required LaTeX packages...")
+  message("Installing tinytex R package...")
   install.packages("tinytex")
+  message("...done.")
+
+  message("Installing tinytex...")
   tinytex::install_tinytex()
+  message("...done.")
+
+  message("Installing required LaTeX packages...")
   tinytex::tlmgr_install(pkgs)
   message("...done.")
 }
