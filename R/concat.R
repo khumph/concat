0b008d268e803948527611aa6ace97e484cc312f
Kyle Humphrey <khumph2@gmail.com>
2022-03-03 14:09:45 -0800

    Build report at least twice so page numbers in multi-page TOCs are correct

--- R/concat.R
+++ R/concat.R
@@ -7,9 +7,12 @@
 #'   E.g., generated using \code{\link[concat]{add_cover_tex}}
 #' @param path_tex The path to output the LaTeX document
 #' @param path_pdf The path to output the PDF document
+#' @param min_times The minimum and maximum number of times to rerun the LaTeX
+#'   engine when using emulation. Needs to be at least 2 for correct page
+#'   numbers in multi-page tables of contents.
 #' @param ... Further arguments passed to \code{\link[tinytex]{pdflatex}}
 #' @export
-concat <- function(path_tlf_csv, toc = TRUE, coverpage = NULL, path_tex, path_pdf, ...) {
+concat <- function(path_tlf_csv, toc = TRUE, coverpage = NULL, path_tex, path_pdf, min_times = 2, ...) {
   build_tex(path_tlf_csv, toc, coverpage, path_tex)
-  tinytex::pdflatex(path_tex, pdf_file = path_pdf, ...)
+  tinytex::pdflatex(path_tex, min_times = min_times, pdf_file = path_pdf, ...)
 }
